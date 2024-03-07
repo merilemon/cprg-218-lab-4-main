b1f6e7d1c8de78c343a8e823b3a785ee11143217
merilemon <147794695+merilemon@users.noreply.github.com>
2024-03-06 19:54:04 -0700

    accordion

---
+++ java/script.js
@@ -0,0 +1,13 @@
+var accordions = document.querySelectorAll('.accordion');
+
+  accordions.forEach(function(accordion) {
+    accordion.addEventListener('click', function() {
+      this.classList.toggle('active');
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + 'px';
+      }
+    });
+  });
