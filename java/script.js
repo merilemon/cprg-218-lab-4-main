14e9661a1e3560c803858607cf079c5d4d292b90
merilemon <147794695+merilemon@users.noreply.github.com>
2024-03-07 01:01:41 -0700

    footer

--- java/script.js
+++ java/script.js
@@ -1,3 +1,11 @@
+/* footer date */
+
+const d = new Date()
+let year = d.getFullYear
+document.getElementById ("yr").innerHTML = year
+
+
+/* accordion button changing */ 
 document.addEventListener('DOMContentLoaded', function() {
     var summaries = document.querySelectorAll('details summary');
 
@@ -6,10 +14,11 @@ document.addEventListener('DOMContentLoaded', function() {
         var icon = this.querySelector('i');
 
         if (this.parentElement.open) {
-          icon.className = 'fas fa-plus'; // Change to plus icon when closed
+          icon.className = 'fas fa-plus'; /* Change to plus icon when closed */
         } else {
-          icon.className = 'fas fa-minus'; // Change to minus icon when open
+          icon.className = 'fas fa-minus'; /* Change to minus icon when open */
         }
       });
     });
   });
+
